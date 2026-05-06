2384f2cc1e961bead8ea89d1a4eb8a03a867fd0f
Felipe Fazio <felipefazio.costa@gmail.com>
2026-05-06 13:57:29 -0300

    bug(vercel):final app

--- api/index.js
+++ api/index.js
@@ -1,1 +1,10 @@
-export { default } from '../server/src/server.js';
+let cachedHandler;
+
+module.exports = async function handler(req, res) {
+	if (!cachedHandler) {
+		const serverModule = await import('../server/src/server.js');
+		cachedHandler = serverModule.default;
+	}
+
+	return cachedHandler(req, res);
+};
